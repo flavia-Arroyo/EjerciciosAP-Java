ab0e262990dd9cdc41cb1d5067d00eb0ef9825c2
flavia-Arroyo <flavia.e.arroyo@gmail.com>
2023-03-05 13:56:44 -0300

    se incorporo ejercicios de la clase 1

---
+++ EjerciciosAP4.0/src/clase1/Ejercicio1.java
@@ -0,0 +1,80 @@
+package clase1;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		/*System.out.println("Ejercicio 1a");
+		
+		int numeroInicio= 5;
+		int numeroFin= 14;
+		while(numeroInicio <= numeroFin) {
+		  System.out.println(numeroInicio);
+			numeroInicio++;
+		}
+		
+		int numeroInicio= 5;
+		int numeroFin= 14;
+		
+		System.out.println("Ejercicio 1b- numeros pares");
+		while(numeroInicio <= 	numeroFin) {
+			if(numeroInicio%2== 0) {
+				System.out.println(numeroInicio);
+				
+			}
+			numeroInicio++;
+			
+			
+		
+		}
+		System.out.println("ejercicio 1c");
+		int numeroInicio= 5;
+		int numeroFin= 14;
+		boolean mostrarPares = false;
+		while(numeroInicio <= 	numeroFin) {
+			if( mostrarPares) {
+				if(numeroInicio%2 == 0) {
+					System.out.println(numeroInicio);
+					
+				}	 
+			
+				
+			}else {
+				if(numeroInicio%2 != 0) {
+
+					System.out.println(numeroInicio);
+					
+				}
+			}
+			numeroInicio++;
+			
+			
+		
+		}*/
+		System.out.println("ejercicio 1d");
+	
+		int numeroFin= 5;
+		
+		for(	int numeroInicio= 14;numeroInicio>= numeroFin; numeroInicio--) {
+			if(numeroInicio%2== 0) {
+				System.out.println(numeroInicio);
+			}
+			
+		}
+		
+		
+		
+		
+		
+			
+			
+		
+		
+		
+		
+		
+	
+
+
+	}
+
+}
---
+++ EjerciciosAP4.0/src/clase1/Ejercicio2.java
@@ -0,0 +1,23 @@
+package clase1;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		float ingresos= 387983;
+		int vehiculo = 2;
+		int inmueble = 2;
+		int embarcacion= 0;
+		int aeronave = 0;
+		boolean socio = false;
+		
+		if(ingresos >= 489083 || vehiculo >= 3|| inmueble>= 3||embarcacion >=1||aeronave>=1 || socio) {
+			System.out.println("Usted pertenece al segmentos de ingresos altos");
+		}else {
+			System.out.println("Usted NO pertenece al segmento de ingresos altos ");
+		}
+		
+	
+
+	}
+
+}
